461e17bdb9bd1bb6406fd547b92f246562aa5cff
Mohd Hussain <samsteve124@gmail.com>
2019-10-24 15:41:48 +0530

    finally reduxer done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import React, { Component } from 'react'
+import {connect} from 'react-redux'
+import Form from './components/Form'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+
+    }
+  }
   render() {
     return (
       <div className="container">
         <div className="row">
-          <h1>You Are </h1>
+          <h1>You Are {this.props.authorised}</h1>
+          <Form />
         </div>
       </div>
     )
   }
 }
-export default App
+
+const mapStateToProps = state => {
+  return {
+    isLoggedIn: state.isLoggedIn,
+    authorised: state.authorised
+  }
+}
+
+export default connect(mapStateToProps)(App)
